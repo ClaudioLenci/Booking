8856450fbeca5f428a443a450cef92e3c52f35cd
LenciCla <lenciclaudio06@gmail.com>
2023-05-15 09:40:16 +0200

    #2 aggiunte classi Prenotazione e Calendario in entrambi i progetti

---
+++ Cliente/Prenotazione.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cliente
+{
+    public class Prenotazione
+    {
+        public string nome { get; set; }
+        public DateTime data_nascita { get; set; }
+        public string tipologia { get; set; }
+        public DateTime inizio { get; set; }
+        public int durata { get; set; }
+        public string altro { get; set; }
+
+        public DateTime fine
+        {
+            get
+            {
+                DateTime fine = inizio;
+                fine.AddHours(durata);
+                return fine;
+            }
+        }
+
+        public Prenotazione(string nome, DateTime data_nascita, string tipologia, DateTime inizio, int durata, string altro)
+        {
+            this.nome = nome;
+            this.data_nascita = data_nascita;
+            this.tipologia = tipologia;
+            this.inizio = inizio;
+            this.durata = durata;
+            this.altro = altro;
+        }
+    }
+}
---
+++ Dottore/Calendario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dottore
+{
+    public class Calendario
+    {
+        public List<Prenotazione> prenotazioni { get; }
+
+        public Calendario()
+        {
+            prenotazioni = new List<Prenotazione>();
+        }
+
+        public List<Prenotazione> GetPrenotazioni()
+        {
+            return prenotazioni.OrderBy(p => p.inizio).ToList();
+        }
+
+        public List<Prenotazione> GetPrenotazioniFuture()
+        {
+            return prenotazioni.OrderBy(p => p.inizio).Where(p => p.inizio >= DateTime.Now).ToList();
+        }
+
+        bool Libero(Prenotazione p)
+        {
+            int n = prenotazioni.Count(p2 => (p2.inizio > p.inizio && p2.inizio < p.fine) || (p2.fine > p.inizio && p2.fine < p.fine));
+            return n == 0;
+        }
+    }
+}
